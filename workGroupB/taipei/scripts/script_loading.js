0ac978bf9a084597b44b897f845c38d98145f11b
ctxx-10 <cjd6txj1y@gmail.com>
2026-02-26 10:01:50 +0900

    Delete workGroupB/taipei/scripts/script_loading.js
    不要ファイル「script_loading.js」削除

--- workGroupB/taipei/scripts/script_loading.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-    const loading = document.getElementById("logoLoading");
-
-    // 点滅3回分でローディング終了
-    setTimeout(() => {
-        loading.classList.add("hide");
-
-    }, 9000);
-});
