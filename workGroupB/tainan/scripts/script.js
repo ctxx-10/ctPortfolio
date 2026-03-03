ec2a15e61fb381cf87d7532888c252d5b46385a4
ctxx-10 <cjd6txj1y@gmail.com>
2026-03-03 15:32:59 +0900

    架空観光サイト/台南ページのアップロード
    台南ページのアップロード
    ・デザインの微調整
    ・モーダル表示位置変更

--- workGroupB/tainan/scripts/script.js
+++ workGroupB/tainan/scripts/script.js
@@ -1,5 +1,11 @@
 'use strict';
 
+// ボタンとモーダル要素を取得
+let modal = document.getElementById("myModal");
+let openBtn = document.getElementById("openModalBtn");
+let closeBtn = document.getElementsByClassName("closeBtn")[0];
+let modalContent = document.querySelector(".modalContent"); //追加
+
 // モーダルウィンドウ処理
 // ボタンとモーダル要素を取得し、モーダル内容を書き換える
 const modalA = document.getElementById('goulmetSec');
@@ -27,15 +33,23 @@ modalButtons.forEach(button => {
             console.log("終了処理");
             break;
     }
+
+    //追加
+    modalContent.classList.remove("leftPos", "rightPos");
+    if (window.innerWidth >= 768) {
+      //PCのみ対応
+      if (buttonValue === '1' || buttonValue ==='3') {
+        //'左側'レイアウト：モーダル'右'表示
+        modalContent.classList.add("rightPos");
+      } else {
+        //'右側'レイアウト：モーダル'左'表示
+        modalContent.classList.add("leftPos");
+      }
+    }
     // ボタンがクリックされたらモーダルを開く
     modal.style.display = "block";
   });
 });
-
-// ボタンとモーダル要素を取得
-let openBtn = document.getElementById("openModalBtn");
-let modal = document.getElementById("myModal");
-let closeBtn = document.getElementsByClassName("closeBtn")[0];
 
 // バツ印がクリックされたらモーダルを閉じる
 closeBtn.onclick = function() {
